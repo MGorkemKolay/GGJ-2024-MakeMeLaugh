6972275639441822a24b0dc30310f7978c9bf19f
Görkem <kolaygorkem@icloud.com>
2024-01-28 12:19:40 +0300

    Dead trigger fix

--- Assets/Scripts/DeadTrigger.cs
+++ Assets/Scripts/DeadTrigger.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class DeadTrigger : MonoBehaviour
 {
     [SerializeField] Transform SpawnPoint;
-
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.name == "SUKRU")
+        if (col.gameObject.name == "Player")
         {
             col.transform.position = SpawnPoint.position;
         }
